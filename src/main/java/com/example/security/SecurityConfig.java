3286471166f1b0e52e6cfb0ccb956696e31946a5
KangSungHyuk <kangsh9820@naver.com>
2023-01-27 12:44:26 +0900

    시큐리티 설정 수정

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -12,14 +12,18 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(authorize -> authorize
-                        .antMatchers("/","/logintest").permitAll() // 인증없이 들어갈 수 있는 주소 설정
+                        .antMatchers("/").permitAll() // 인증없이 들어갈 수 있는 주소 설정
                         .anyRequest().authenticated() // 그외 나머지 주소는 인증 필요
                 );
 
         http
-                .formLogin(login->login.loginPage("/logintest"));
-                //.formLogin(withDefaults())
-                //.httpBasic(withDefaults());
+                .formLogin(login ->
+                        login
+                                .loginPage("/logintest")
+                                .permitAll()
+                );
+        //.formLogin(withDefaults())
+        //.httpBasic(withDefaults());
         return http.build();
     }
 }
