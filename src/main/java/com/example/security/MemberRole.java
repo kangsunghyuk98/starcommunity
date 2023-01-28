3837aff7e56c73073a2a3d6d9cf7d51a21e66907
KangSeongHyeok <sung9820@naver.com>
2023-01-28 15:39:03 +0900

    복구

--- src/main/java/com/example/security/MemberRole.java
+++ src/main/java/com/example/security/MemberRole.java
@@ -9,6 +9,7 @@ public enum MemberRole {
     USER("ROLE_USER","회원"),
     ADMIN("ROLE_ADMIN","관리자");
     // 일반사용자와 관리자 권한 분리 USER : 0, ADMIN : 1
+    //ergergerg
 
     final String role; // 생성자 주입을 통해 나중에 권한이 추가되더라도 노가다 안해도됨
     final String koRole; // 회원, 관리자
