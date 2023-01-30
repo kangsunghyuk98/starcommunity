5107344a38c15230cafa40a43b410c9d227eaf42
Danielpp861 <danielpp861@gmail.com>
2023-01-30 21:36:26 +0900

    디자인 작업

---
+++ src/main/resources/static/js/(1.3.1.3)boardmodify.js
@@ -0,0 +1,18 @@
+
+// 스마트에디터 설정입니다.
+/*
+let oEditors = []
+
+smartEditor = function () {
+    nhn.husky.EZCreator.createInIFrame({
+        oAppRef: oEditors,
+        elPlaceHolder: "editorTxt",
+        sSkinURI: "smarteditor/SmartEditor2Skin.html",
+        fCreator: "createSEditor2"
+    });
+} 
+
+$(document).ready(function () { 
+    smartEditor()
+});
+*/
