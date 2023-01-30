45a61d5667668d4e45acdd31d6d9db6680cf91f0
Danielpp861 <danielpp861@gmail.com>
2023-01-30 14:17:47 +0900

    header.js 주석처리

--- design01/js/(1)header.js
+++ design01/js/(1)header.js
@@ -1,7 +1,7 @@
-const adminInfo = 1;
+// const adminInfo = 1;
 
-$(document).ready(function() {
-    if( adminInfo == 1 ) {
-        $('.memberShip').append('<button type="button" href="#" class="btn">회원 관리</button>');
-    }
-});
+// $(document).ready(function() {
+//     if( adminInfo == 1 ) {
+//         $('.memberShip').append('<button type="button" href="#" class="btn">회원 관리</button>');
+//     }
+// });
